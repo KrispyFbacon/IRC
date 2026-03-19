6a1f5847bdef0627ced11c09daa83fbc5b238463
Francisco Antunes <71651745+KrispyFbacon@users.noreply.github.com>
2026-03-19 19:11:50 +0000

    Update JoinCommand

--- inc/Commands/ChannelCommands/JoinCommand.hpp
+++ inc/Commands/ChannelCommands/JoinCommand.hpp
@@ -9,7 +9,8 @@ class Client;
 class JoinCommand : public ACommand
 {
 	private:
-
+		void	joinChannel(Server& server, Client& client, const std::string& channelName, const std::string& key);
+		
 	public:
 		static ACommand* make() { return new JoinCommand(); }
 
--- src/Commands/ChannelCommands/JoinCommand.cpp
+++ src/Commands/ChannelCommands/JoinCommand.cpp
@@ -5,50 +5,25 @@ void JoinCommand::execute(Server& server, Client& client, const Message& msg)
 {
 	Print::Debug ("JOIN COMMAND CALLED!");
 
-	//TODO Registered?
-	//TODO not enough params		-> ERR_NEEDMOREPARAMS
+	//TODO Registered? ✅ Done before execute
+
+	if (msg.params.empty() || msg.params[0].empty())
+        return(sendError(client, IRC::ERR_NEEDMOREPARAMS, "JOIN :Not enough parameters"));
+
 	//TODO Split argument with ',' channel/key
-	//TODO isValidChannelName		-> ERR_NOSUCHCHANNEL
-	//TODO GetChannel
-		// Channel exists?
-			// Create Channel
-			// Channel limit?
-		//else
-			// already in channel?	-> ERR_USERONCHANNEL
-			// is invite only?		-> ERR_INVITEONLYCHAN
-			// has Password?		-> ERR_BADCHANNELKEY
-			// is it Full?			-> ERR_CHANNELISFULL
-			// channel.AddClient(client)?
-			// client.sendMessage(joinMessage)?
-			// channel.broadcast(joinMessage)?
 
-		// TOPIC EMPTY?
-			// sendReply(client, RPL_TOPIC, channelName + " :" + channel->getTopic());
-		//else
-			// sendReply(client, RPL_NOTOPIC, channelName + " :No topic is set");
-		
-		// send Channel List?
-			// channel or client exist?
-				//return
-			// loop throw channel -> user
-				// is channel->isModerator / find in channel._moderators
-				// get nickName();
-			// Send List
-				// sendReply(client, IRC::RPL_NAMREPLY, "= " + channel->getName() + " :" + list);
-				// sendReply(client, IRC::RPL_ENDOFNAMES, channel->getName() + " :End of NAMES list");
+	for (size_t i = 0; i < channels.size(); i++)
+	{
+		joinChannel(client, channels[i], (i >= keys.size() ? "" : keys[i]));
+	}
 
 
-
-	// TODO 
-	// if (!client.isRegistered()) {
-	// 	sendError(client, IRC::ERR_NOTREGISTERED, "JOIN :You have not registered");
-	// 	return;
-	// }
 
 	// TODO prefix client?  return ":" + client.username
 	// TODO WE HAVE PREFIX client.getPrefix();
-	(void)server;
-	client.sendMessage(":" + client.getUsername() + " JOIN " + "#" + msg.params[0]); // TODO channel name // msg.params[1];
+
+	//(void)server;
+	//client.sendMessage(":" + client.getUsername() + " JOIN " + "#" + msg.params[0]); // TODO channel name // msg.params[1];
 	//std::string joinMessage =":" + client->getNickname() + " JOIN :" + channelName";
 
 	//TODO BRODCAST MESSAGE(client, channel, "brodcast message")?
@@ -67,9 +42,58 @@ void JoinCommand::execute(Server& server, Client& client, const Message& msg)
 	
 }
 
-// TODO ALL ERRORS
-// ERR_NEEDMOREPARAMS	ERR_BANNEDFROMCHAN
-// ERR_INVITEONLYCHAN	ERR_BADCHANNELKEY
-// ERR_CHANNELISFULL	ERR_BADCHANMASK
-// ERR_NOSUCHCHANNEL	ERR_TOOMANYCHANNELS
-// RPL_TOPIC
+
+void	JoinCommand::joinChannel(Server& server, Client& client, const std::string& channelName, const std::string& key)
+{
+	//TODO isValidChannelName		-> ERR_NOSUCHCHANNEL
+		// TODO
+		// <2 - FALSE
+		// channelName[0] != '#' && channelName[0] != '&'?
+		// Check for invalid characters in channel name
+	if (isValidChannelName(const std::string& channelName) cons)) // in Acommand?
+		return(sendError(client, IRC::ERR_NOSUCHCHANNEL, channelName + " :No such channel"));
+
+	//TODO GetChannel
+	Channel* channel = server.getChannel(channelName);
+
+		// Channel exists?
+		if(!channel)
+		{
+			// -> Create new Channel
+            // -> Add Channel to Server's map
+            // -> channel.addClient(client)
+            // -> channel.addModerator(client) // Creator becomes moderator
+            // -> client.addChannel(channel)
+		}
+		//else
+		{
+			// already in channel?	-> ERR_USERONCHANNEL
+			// is invite only?		-> ERR_INVITEONLYCHAN
+			// has Password?		-> ERR_BADCHANNELKEY
+			// is it Full?			-> ERR_CHANNELISFULL
+			// channel.AddClient(client)?
+			// -> chan.removeInvite(client.getNickname()) <-- Consume ticket!
+			// client.sendMessage(joinMessage)?
+		}
+
+		//TODO broadcast channel
+		// chan.broadcast(":" + client.getNickName() + " JOIN :" + chanName); // client.getPrefix()?
+
+		//TODO send topic
+			if (chan.getTopic().empty())
+				sendReply(client, IRC::RPL_NOTOPIC, channelName + " :No topic is set");
+			else
+				sendReply(client, RPL_TOPIC, channelName + " :" + channel->getTopic());
+		
+		// send Channel List?
+			// channel or client exist?
+				//return
+			// loop throw channel -> user
+				// is channel->isModerator // find in channel._moderators add '@'
+				// get nickName();
+			// Send List
+				// sendReply(client, IRC::RPL_NAMREPLY, "= " + channel->getName() + " :" + list);
+				// sendReply(client, IRC::RPL_ENDOFNAMES, channel->getName() + " :End of NAMES list");
+	
+
+}
